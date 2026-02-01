b9e19aa302637cf5002a9df5e4da093e7c9a5608
murilo <murilo02silva@gmail.com>
2026-02-01 17:31:24 -0300

    titulo sendo puxado

--- src/App.js
+++ src/App.js
@@ -2,22 +2,39 @@
 import './App.css';
 import {db} from './firebase.js';
 import {useEffect, useState} from 'react';
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import Header from './Header.js';
 
 function App() {
 
   const [user, setUser] = useState('');
+  const [posts, setPosts] = useState([]);
 
-  useEffect(()=>{
-    console.log(db);
-  },[])
+useEffect(() => {
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+        setPosts(snapshot.docs.map(doc => ({
+            id: doc.id,
+            info: doc.data()  
+        })));
+    });
+
+    return () => unsubscribe(); 
+}, []);
+
 
   return (
     <div className="App">
 
       <Header setUser={setUser} user={user}></Header>
 
-
+      {
+        posts.map(function(val) {
+        return (
+      <p>{val.info?.titulo}</p> 
+    )
+  })
+      }
 
     </div>
   );
